9dab057f6036efee29dc2df6f5203610393a9737
DaniDevelopment98 <Jorged1923>
2022-07-19 13:45:07 -0600

    second commit

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<mat-toolbar color="primary">
+  <span>Store</span>
+</mat-toolbar>`,
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent  {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
